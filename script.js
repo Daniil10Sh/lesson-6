ed8d7ce536db46f126dd48463c1078e9247a62ab
Daniil10Sh <danik2022odessa@gmail.com>
2024-03-25 16:53:39 +0200

    add settings

---
+++ script.js
@@ -0,0 +1,17 @@
+// let arr = ['Mango  ', 'Poly  ', 'Kiwi  ', 'Ajax'];
+// let combined = arr.join("");
+// console.log(combined);
+
+
+let arr = [
+    'Карточка-1',
+    'Карточка-2',
+    'Карточка-3',
+    'Карточка-4',
+    'Карточка-5',
+  ];
+//  let slice = arr.slice(1);
+//  console.log(slice)
+// let plus = arr.push( 'Карточка-6');
+// console.log(arr);
+// console.log(plus);
